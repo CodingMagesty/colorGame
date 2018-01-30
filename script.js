6fc7231b1a062dbb220e5c252b176b0915a9ad17
CodingMagesty <ermolaevGleb@outlook.com>
2018-01-30 17:09:25 +0300

    Color randomization is added, color now changes for h1 if guessed correctly

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)"
-]
-
+var colors = generateRandomColors(6);
+var message = document.querySelector("#message");
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
+var h1 = document.querySelector("h1");
 
 for (var i = 0; i < squares.length; i++) {
-  //add initial colors
+  //adds initial colors
   squares[i].style.backgroundColor = colors[i];
-  //add event listeners
+  //adds event listeners
   squares[i].addEventListener("click", function() {
-    //grab color from square
+    //grabs color from square
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
-      alert("true!");
+      message.textContent = "Correct!"
+      h1.style.backgroundColor = clickedColor;
+      changeColor(clickedColor);
     } else {
-      alert("false!");
+      message.textContent = "Try Again!"
+      this.style.background = "#232323"
     }
   });
+}
+
+function changeColor(color) {
+  //loop through squares to match color
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  //creates array of num colors
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    //gets and pushes random color to array
+    arr.push(randomColor());
+  }
+
+  return arr;
+}
+
+function randomColor() {
+  //generates random color
+  var g = Math.floor(Math.random() * 256);
+  var r = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+
+  var rgb = "rgb(" + r + ", " + g + ", " + b + ")";
+  console.log(rgb);
+  return rgb;
 }
